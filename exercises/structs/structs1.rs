7d545f0d9c637a91039d9db5f0e6f3ccddc5a775
Richard Hill <ricjhill@protonmail.com>
2020-06-25 12:00:33 +0200

    Finished some  rustlings exercises

--- exercises/structs/structs1.rs
+++ exercises/structs/structs1.rs
@@ -1,13 +1,16 @@
 // structs1.rs
 // Address all the TODOs to make the tests pass!
 
-// I AM NOT DONE
 
-struct ColorClassicStruct {
+
+struct ColorClassicStruct<'a> {
+    name: &'a str,
+    hex:&'a str,
     // TODO: Something goes here
+
 }
 
-struct ColorTupleStruct(/* TODO: Something goes here */);
+struct ColorTupleStruct( String, String);
 
 #[derive(Debug)]
 struct UnitStruct;
@@ -15,11 +18,15 @@ struct UnitStruct;
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::string::ToString;
 
     #[test]
     fn classic_c_structs() {
         // TODO: Instantiate a classic c struct!
-        // let green =
+        let name ="green";
+        let hex =  "#00FF00";
+         let green =  ColorClassicStruct { name, hex};
+
 
         assert_eq!(green.name, "green");
         assert_eq!(green.hex, "#00FF00");
@@ -28,8 +35,9 @@ mod tests {
     #[test]
     fn tuple_structs() {
         // TODO: Instantiate a tuple struct!
-        // let green =
-
+        let colour :String = "green".to_string();
+        let hex: String =  "#00FF00".to_string();
+        let green = ColorTupleStruct(colour, hex);
         assert_eq!(green.0, "green");
         assert_eq!(green.1, "#00FF00");
     }
@@ -37,7 +45,7 @@ mod tests {
     #[test]
     fn unit_structs() {
         // TODO: Instantiate a unit struct!
-        // let unit_struct =
+        let unit_struct = UnitStruct;
         let message = format!("{:?}s are fun!", unit_struct);
 
         assert_eq!(message, "UnitStructs are fun!");
--- exercises/test1.rs
+++ exercises/test1.rs
@@ -7,10 +7,17 @@
 // more than 40 at once, each apple only costs 1! Write a function that calculates
 // the price of an order of apples given the order amount. No hints this time!
 
-// I AM NOT DONE
+
+
 
 // Put your function here!
-// fn ..... {
+ fn calculate_apple_price(num :i32) -> i32 {
+  let  mut price=0;
+if num  > 40 {
+price = num
+} else {price = num*2}
+return price
+}
 
 // Don't modify this function!
 #[test]
