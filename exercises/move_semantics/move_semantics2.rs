136b8f4439a88cf63c3aa7451ae529a77afba006
Richard Hill <ricjhill@protonmail.com>
2020-08-13 13:07:01 +0200

    Finished some  rustlings quiz about ownership. I  used this https://doc.rust-lang.org/book/ch04-02-references-and-borrowing.html. What  happens is that  when  a thing is out of scope  it  is dropped. When two ref on the  stack point  to the  same thing on the  heap,  then the  first  ref is replced by  the  second ref. THis is called a "move".  The first  ref is now  invalid and throws an error
      `rustc --explain E0382`.
    
     The error can be fixed by  using clone() method which  acts as  a deep copy.
    
        |
     9  |     let vec0 = Vec::new();
        |         ---- move occurs because `vec0` has type `std::vec::Vec<i32>`, which does not implement the `Copy` trait
     11 |     let mut vec1 = fill_vec(vec0);
        |                             ---- value moved here
     ...
     14 |     println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
        |                                                         ^^^^ value borrowed here after move

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -5,9 +5,10 @@
 // I AM NOT DONE
 
 fn main() {
+
     let vec0 = Vec::new();
 
-    let mut vec1 = fill_vec(vec0);
+    let mut vec1 = fill_vec(vec0.clone());
 
     // Do not change the following line!
     println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
@@ -16,6 +17,7 @@ fn main() {
 
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 }
+
 
 fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
     let mut vec = vec;
