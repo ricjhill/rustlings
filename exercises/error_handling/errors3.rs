751fe576f091b1bf87f381dd1de9fb12fb98927f
Richard Hill <ricjhill@protonmail.com>
2020-08-19 22:53:11 +0200

    Finished  error3.. used match for  to replace if and made Main return a Result type and a parseError.  I need to look at this again. I dont fully  understand it.

--- exercises/error_handling/errors3.rs
+++ exercises/error_handling/errors3.rs
@@ -4,22 +4,22 @@
 // Why not? What should we do to fix it?
 // Execute `rustlings hint errors3` for hints!
 
-// I AM NOT DONE
 
 use std::num::ParseIntError;
 
-fn main() {
+fn main() -> Result<(),ParseIntError> {
     let mut tokens = 100;
     let pretend_user_input = "8";
 
-    let cost = total_cost(pretend_user_input)?;
+    let cost = total_cost(pretend_user_input);
+    match cost {
+        Ok(x) if x > tokens => println!("You can't afford that many!"),
+        Ok(x) => {
+            tokens -=x;
+            println!("You now have {} tokens.", tokens);
+        },Err(e) => (),
+    }Ok(())
 
-    if cost > tokens {
-        println!("You can't afford that many!");
-    } else {
-        tokens -= cost;
-        println!("You now have {} tokens.", tokens);
-    }
 }
 
 pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
@@ -28,4 +28,5 @@ pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     let qty = item_quantity.parse::<i32>()?;
 
     Ok(qty * cost_per_item + processing_fee)
+
 }
